7f84dc36d25e249931b95cfa1f8b4a78471c5890
yahya.codin <yahya.codin@gmail.com>
2022-10-07 09:09:05 +0300

    m

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,19 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import classes from "./NavigationBar.module.css";
-import images from "../images/tiens-group-logo.png";
 
 const Navigationbar = () => {
   return (
     <div>
       <nav className={classes.navbar}>
         <Link to="/">
-          <img src={images} alt="logo" className={classes.image} />
-        </Link>
-        <Link to="/">
           {" "}
           <label htmlFor="logo-label" className={classes.logoLabel}>
-            TIENS
+            SAID
           </label>{" "}
         </Link>
         <ul className={classes.navbar}>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
 import React from "react";
 
+import classes from "./Home.module.css";
+
 const HomePage = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className={classes.quote}>
+        <h1 className={classes.phrase}>Tiens Goup Distributor</h1>
+        <p className={classes.advertise}>
+          Finding TIENS is the initial step to changing your life and that of
+          your loved ones. <br /> We all, search for an ideal way of life -
+          which includes no cash stresses, no pressure, <br /> and a lot of
+          holiday occasions. Accomplishing your fantasies is currently most
+          certainly <br /> conceivable in light of the fact that joining TIENS
+          will give you a guide to making
+          <br /> them a reality! There are many advantages of joining the TIENS
+          business.
+        </p>
+      </div>
+      {/* Interesting items in view of 5000 years of Conventional Chinese Medication
+      joined with current exploration, <br />
+      science, and innovation. Top-notch Schooling and Preparing Framework which
+      helps guide you to accomplish <br /> a maintainable, fruitful business
+      with us. Optimistic acknowledgment levels. */}
     </div>
   );
 };
